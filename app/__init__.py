68a1ed741504af16a4255f3521f86ed69d0fb6dd
michael <mikelkarije@gmail.com>
2019-11-22 16:59:02 +0300

    configured the the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
+
+app.config.from_object(DevConfig)
+app.config.from_pyfile("config.py")
+
+
+bootstrap = Bootstrap(app)
+...

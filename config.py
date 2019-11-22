def096fd94307bdc556b93546761d2aa2fbd3909
michael <mikelkarije@gmail.com>
2019-11-22 16:26:38 +0300

    this commit adds our configurations of the application in order for it to run

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+    pass
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG=True
+    
+config_options ={
+    'production' : ProdConfig,
+    'development': DevConfig
+}

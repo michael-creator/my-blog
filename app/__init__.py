2bc5c94a65b2826d4a7a9d0dbe334e70948a9deb
michael <mikelkarije@gmail.com>
2019-11-24 15:28:06 +0300

    initialized bootstrap in our application

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,10 @@ from config import config_options
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
- 
+    
+    
+    bootstrap = Bootstrap(app)
+    
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 

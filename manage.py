04337c23e1dcfbb3b9ca3734899c92b5d5ebb5ae
michael <mikelkarije@gmail.com>
2019-11-24 23:23:42 +0300

    this commit adds a secret key to make the app more secure

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager,Shell,Server
 from app import create_app
 
 app=create_app('development')
-
+# app.config('SECRET_KEY')='0974cf42c1789aaa8312bf9e8ab3af3e'
 manager = Manager(app)
 manager.add_command('Server',Server)
 

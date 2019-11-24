6456448b34c3b7fe63c3f6f1aa24664900aba5cd
michael <mikelkarije@gmail.com>
2019-11-24 14:45:42 +0300

    importation of main blueprint and registered it in the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
+ 
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+   
+    return app
 
-
-bootstrap = Bootstrap(app)
-...

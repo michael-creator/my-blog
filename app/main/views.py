5f05032962702e31804d314dd43444f80d832a6b
michael <mikelkarije@gmail.com>
2019-11-24 15:26:16 +0300

    here i have imported views and the main blueprint

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,28 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from . import main 
 
+
+posts = [
+    {
+        'author': 'mikel karije',
+        'title': 'funny blog',
+        'content': '1blog  content',
+        'date_posted': 'april 22, 2019'
+    },
+     {
+        'author': 'mikel karije',
+        'title': 'social blog',
+        'content': '2blog  content',
+        'date_posted': 'april 22, 2019'
+    },
+      {
+        'author': 'john dumelo',
+        'title': 'business blog',
+        'content': '3blog  content',
+        'date_posted': 'april 22, 2019'
+    }
+]
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)

b9837639252c9dfcd40cfeafc33e53a7b70b5b5a
michael <mikelkarije@gmail.com>
2019-11-25 11:22:31 +0300

    in the class for login i have i have inserted inputs for users to add when they are signing in like email and password

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StrinField ,PasswordField ,SubmitField, BooleanField
+from wtforms import StringField ,PasswordField ,SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StrinField('username', validators=[DataRequired(),Length(min=4, max = 18)])
-    email = StrinField('Email', validators=[DataRequired(),Email()])
-    password = passwordField('password', validators=[DataRequired()])
-    confirm_password = passwordField('confirm_password', validators=[DataRequired(),EqualTo('password')]) 
+    username = StringField('username', validators=[DataRequired(),Length(min=4, max = 18)])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('password', validators=[DataRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[DataRequired(),EqualTo('password')]) 
     submit = SubmitField('sign up') 
     
 class LoginForm(FlaskForm):
-    email = StrinField('Email', validators=[DataRequired(),Email()])
-    password = passwordField('password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('login') 
     

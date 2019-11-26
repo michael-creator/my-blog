30d3454c6a44537db214b1ee8e268fc11eaad000
michael <mikelkarije@gmail.com>
2019-11-26 16:59:41 +0300

    initial comit to heroku

--- manage.py
+++ manage.py
@@ -2,14 +2,13 @@ from flask_script import Manager,Shell,Server
 from app import create_app ,db
 from app.models import User
 from  flask_migrate import Migrate, MigrateCommand
-
-
 app=create_app('production')
 manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 manager.add_command('Server',Server)
 @manager.shell
+
 def make_shell_context():
     return dict(app = app,db = db,User = User )
 

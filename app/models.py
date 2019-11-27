5673182edb38617da3076d1fd4448a68f73587f3
michael <mikelkarije@gmail.com>
2019-11-27 11:39:57 +0300

    deletion of role.id since i have a role class

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(280))
     email = db.Column(db.String(255),unique = True,index = True)
-    # role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
@@ -36,7 +36,7 @@ class User(UserMixin,db.Model):
     
     
 class Role(db.Model):
-    __tablename__ = 'roles'
+    __tablename__ = 'role'
     id = db.Column(db.Integer, primary_key= True)
     name = db.Column(db.String(255))
     users = db.relationship('User', backref = 'role', lazy="dynamic")

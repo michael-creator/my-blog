c2b2b12537d819b07348004e5cafc29cb63a7a28
michael <mikelkarije@gmail.com>
2019-11-27 11:04:40 +0300

    initial commit

--- app/auth/views.py
+++ app/auth/views.py
@@ -29,7 +29,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         
-        mail_message("Welcome to MY BLOG POST","email/welcome_user",user.email,user=user)
+        # mail_message("Welcome to MY BLOG POST","email/welcome_user",user.email,user=user)
         
         
         return redirect(url_for('auth.login'))
